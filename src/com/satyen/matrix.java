296f230e6bdef94d007e3981320edbc68eda5db1
Satyen roy <satyenroy333@gmail.com>
2022-07-30 13:50:32 +0600

    Matrix Patter Using for loop in java

---
+++ src/com/satyen/matrix.java
@@ -0,0 +1,22 @@
+package com.satyen;
+
+public class matrix {
+
+    public static void main(String[] args){
+        int m = 3;
+        int n = 4;
+
+
+        for(int i = 1; i <= m; i++){
+            for(int j = 1; j <= n; j++){
+                System.out.print(" 0 ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+
+
+
